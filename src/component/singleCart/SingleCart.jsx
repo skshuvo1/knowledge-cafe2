74dcd157a072c7613f7027651c7b041770c8ee18
S.K Shuvo <zamiulbasedshuvo444@gmail.com>
2023-04-01 14:27:37 +0600

    card added

--- src/component/singleCart/SingleCart.jsx
+++ src/component/singleCart/SingleCart.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import './SingleCart.css'
+
 
 const SingleCart = ({cart}) => {
     console.log(cart);
     return (
-        <div>
-            <img src={cart.picture} alt="" />
+        <div className='main-container'>
+            <div className='cart'>
+            <img className='rounded w-100' src={cart.picture} alt="" />
+
+            <div className="name-container d-flex justify-content-between mt-3">
+            <div className='d-flex'>
+            <div className='author-image'>
+            <img src={cart.authorImg} alt="" />
+            </div>
+            <div className='ms-3 mt-2'>
+            <h3>{cart.name}</h3>
+            <p className = "text-secondary">Mar 24 (8 Days ago)</p>
+            </div>
+            </div>
+            <div className="bookmark">
+                <p>06 min read</p>
+            </div>
+            </div>
+
+            <div className="title mt-3">
+                <h2>{cart.title}</h2> 
+        
+                <a href="#">Mark as read</a>
+                
+            </div>
+            
+                
+            
+            
+        </div>
+        <div className="side-ber">
+            <h2>right side</h2>
+        </div>
         </div>
     );
 };
