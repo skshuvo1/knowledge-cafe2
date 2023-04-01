691a4948a0995ad3bdb061233a000f50d06a5774
S.K Shuvo <zamiulbasedshuvo444@gmail.com>
2023-04-01 07:22:08 +0600

    header added

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/pique.jpg';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='d-flex justify-content-between align-items-center m-4 bg-danger-subtle'>
+            <div className="knoledge-container">
+                <h2 className='ms-2'>Programming World</h2>
+            </div>
+            <div className="nav-img">
+                <img src={logo} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
